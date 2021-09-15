1e59666252f1380afad22a2262eddbe7e44429f7
anonimo223 <anguielugo44@gmail.com>
2021-09-15 15:41:23 -0500

    Subir: se procede a subir la carpeta con los archivos.

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/estilos2.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1 class="p1">Bienvenido al Header</h1>
+    </header>
+
+    <main>
+        <div class="navegation">
+            <nav>
+                <p class="p2">Navegacion</p>
+            </nav>
+        </div> 
+
+        <article>
+            <h2 class="p3">Titulo del post</h2>
+            <p class="p2">Lorem ipsum...
+        </article>
+
+        <aside>
+            <p class="p2">Columna</p>
+        </aside>
+    </main>
+
+    <footer>
+        <p class="p4">Footer</p>
+    </footer>
+</body>
+</html>
